6c6cad16bfe73d6dd9bc277da37dd48cd3ef73b3
Alejandro Moreno Cornejo <morenocornejoalejandro01@gmail.com>
2026-03-16 21:52:29 +0100

    feat: add initial project structure

---
+++ src/main/java/com/mgcss/domain/Solicitud.java
@@ -0,0 +1,38 @@
+package com.mgcss.domain;
+
+import java.time.LocalDateTime;
+
+public class Solicitud {
+
+	private Long id;
+	private Estado estado;
+	private LocalDateTime fechaCreacion;
+
+	public Solicitud() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+}
+
