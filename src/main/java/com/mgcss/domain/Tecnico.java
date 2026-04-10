06444edcef7591cc965386ed73464e158e25ce79
Alejandro Moreno Cornejo <morenocornejoalejandro01@gmail.com>
2026-04-10 14:22:44 +0200

    fix: remove unused attribute to pass coverage test

--- src/main/java/com/mgcss/domain/Tecnico.java
+++ src/main/java/com/mgcss/domain/Tecnico.java
@@ -3,19 +3,10 @@ package com.mgcss.domain;
 public class Tecnico {
 
 	private boolean activo;
-	private String nombre;
 	private int experiencia;
 	
 	public Tecnico() {
 		//Constructor vacío necesario para instanciar
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
 	}
 
 	public boolean isActivo() {
